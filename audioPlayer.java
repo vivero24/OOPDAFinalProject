6a61182efe6cc70853290ad88517f118821e7d24
Cristian Vivero <vivero24@students.rowan.edu>
2024-12-08 13:53:01 -0500

    added audioPlayer because BlueJ said there was no source file

---
+++ audioPlayer.java
@@ -0,0 +1,59 @@
+/*
+ * Implementation by:
+ * @author (Samuel Hopper)
+ * @version (v1.0 04-25-24)  Spring'24
+ */
+import javax.sound.sampled.*;
+import java.io.*;
+
+public class audioPlayer implements Runnable {
+    private String soundFilePath;
+
+    public audioPlayer(String soundFilePath) 
+    {
+        this.soundFilePath = soundFilePath;
+    }
+
+    @Override
+    public void run() 
+    {
+        playSound(soundFilePath);
+    }
+
+    private void playSound(String filePath) 
+    {
+        try 
+        {
+            File soundFile = new File(filePath);
+            AudioInputStream audioStream = AudioSystem.getAudioInputStream(soundFile);
+            Clip clip = AudioSystem.getClip();
+            clip.open(audioStream);
+            clip.start();
+            clip.addLineListener(new LineListener()
+                {
+                    @Override
+                    public void update(LineEvent event) 
+                    {
+                        if (event.getType() == LineEvent.Type.STOP)
+                        {
+                            clip.close();
+                        }
+                    }
+                });
+
+            while (clip.isOpen()) 
+            {
+                if (Thread.currentThread().isInterrupted()) 
+                {
+                    clip.close();
+                    Thread.currentThread().interrupt(); 
+                    return;
+                }
+            }
+        } catch (Exception ex) 
+        {
+            ex.printStackTrace();
+        }
+    }
+}
+
